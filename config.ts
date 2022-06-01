6c6b02173961276ae70cfce3621f8544831ba780
Joao Castro <joaocastrv@gmail.com>
2022-06-01 19:44:41 -0300

    Prod

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 export = {
   // PRODUÇÃO = false
   // HOMOLOGAÇÃO = true
-  sandbox: true,
-  client_id: 'Client_Id_423ad82df6b37d9a55897d69149eaffcd5040462',
-  client_secret: 'Client_Secret_58d2aac4c65b661be0b45e6f396ce1eb13ecb854',
-  pix_cert: './certificado.p12',
+  sandbox: false,
+  client_id: 'Client_Id_92518fb455a96747b9a8bf50c1b7b63d2ce7c774',
+  client_secret: 'Client_Secret_dff853cf9a84c1f8ea7063f56be019b69f954b12',
+  pix_cert: './producao-317768-loquaz-prod.p12',
 }
