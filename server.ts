77178143480152d2c7cb74afc26ef2f2558036f3
Joao Castro <joaocastrv@gmail.com>
2022-06-01 16:44:54 -0300

    Ajuste certificado 2

--- server.ts
+++ server.ts
@@ -13,21 +13,7 @@
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
-import { createServer } from 'https'
-import { join } from 'path'
-import fs from 'fs'
-
-const httpsOptions: any = {
-  cert: fs.readFileSync(join(__dirname + '/sslCert/certificate.crt')), // Certificado fullchain do dominio
-  key: fs.readFileSync(join(__dirname + '/sslCert/private.key')), // Chave privada do domínio
-  ca: fs.readFileSync(join(__dirname + '/sslCert/chain-pix-sandbox.cer')), // Certificado público da Gerencianet
-  minVersion: 'TLSv1.2',
-  requestCert: true,
-  rejectUnauthorized: false, //Mantenha como false para que os demais endpoints da API não rejeitem requisições sem MTLS
-}
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
-new Ignitor(__dirname).httpServer().start((handle) => {
-  return createServer(httpsOptions, handle)
-})
+new Ignitor(__dirname).httpServer().start()
